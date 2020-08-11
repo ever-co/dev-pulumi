75a9660c1919e4a42afb7a2ad4c77f27bf5e8b6d
IsmayilMirzali <ismayilmirzeli@gmail.com>
2020-08-11 22:39:28 +0300

    Move ansible playbooks to separate directory

--- deploy.sh
+++ deploy.sh
@@ -74,7 +74,7 @@ run_playbook() {
     fi
 
     echo -e "${BLUE}Firing Ansible playbook...${NC}"
-    ansible-playbook playbook.yaml -i $(pwd)/hosts --key-file="${ssh_key}"
+    ansible-playbook playbooks/playbook.yaml -i $(pwd)/hosts --key-file="${ssh_key}"
     rm -f $(pwd)/hosts # Get rid of hosts file
 }
 
